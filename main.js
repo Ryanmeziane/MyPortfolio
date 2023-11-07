32bc6b8ac45860c880a4c1c765502ba37aed1f00
Ryan M <77261159+Ryanmeziane@users.noreply.github.com>
2023-11-06 20:52:34 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.style.opacity = 1;
+                entry.target.style.transform = 'translateY(0)';
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    const sections = document.querySelectorAll('.portfolio-section');
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
