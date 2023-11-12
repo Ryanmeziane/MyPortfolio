51bae3ceac7bb709eca4412ec229e546a05f2b78
Ryanmeziane <77261159+Ryanmeziane@users.noreply.github.com>
2023-11-12 17:09:56 -0500

    particle sim added

--- main.js
+++ main.js
@@ -21,9 +21,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const createRain = () => {
         const rainDiv = document.createElement('div');
         rainDiv.classList.add('rain');
-        rainDiv.innerText = Math.random() < 0.5 ? '0' : '1';
-        //rainDiv.style.fontSize = Math.random() * 0.5 + 1 + 'rem';
-        rainDiv.style.left = Math.random() * window.innerWidth + 'px';
+        rainDiv.innerText = Math.random() < 0.5 ? '|' : '|';
+        rainDiv.style.left = Math.random() * (window.innerWidth - 40) + 'px';
         document.body.appendChild(rainDiv);
 
         setTimeout(() => {
@@ -35,12 +34,12 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    const aboutMeText = "\nHello! I’m Ryan Meziane, a computer science enthusiast\n" +
+    const aboutMeText = "\n\nHello! I’m Ryan Meziane, a computer science enthusiast\n" +
         "with a passion for merging the worlds of technology and the cosmos.\n" +
         "As a goal-oriented programmer, I specialize in C#, JavaScript, and \nJava, " +
         "bringing forth a comprehensive skill set and innovative solutions";
     let i = 0;
-    const speed = 50; // Speed of typing, in milliseconds
+    const speed = 20;
     const aboutMeContent = document.getElementById('about-me-content');
     aboutMeContent.innerHTML += "RyanMeziane:/$ cat AboutMe.txt";
 
